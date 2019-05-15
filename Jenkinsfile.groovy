11abb811b19321d9b4e4f813ab0e0abe0250a9e5
Jiangkan Pan <nick.pan@qbe.com>
2019-05-15 11:34:08 +1000

    add deploy to staging

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,5 +12,12 @@ pipeline {
                 }
             }
         }
+        stage('Deploy to Staging'){
+            steps {
+                build job: "deploy-to-staging"
+            }
+
+
+        }
     }
 }
